96dfe097481aa721efb37f7db100d5a4bd5f0549
Yesman <yesman1058@gmail.com>
2026-06-15 21:57:59 +0900

    Made lobby

---
+++ Source/Beadurinc/GameData/LobbyGameMode.cpp
@@ -0,0 +1,2 @@
+#include "GameData/LobbyGameMode.h"
+
---
+++ Source/Beadurinc/GameData/LobbyGameMode.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "LobbyGameMode.generated.h"
+
+UCLASS()
+class BEADURINC_API ALobbyGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+};
